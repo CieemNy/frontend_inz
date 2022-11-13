694434b51d3a560c040ed2716ccae1ec94c7a9c6
Damian Banach <damian070799@wp.pl>
2022-11-13 17:25:50 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Register/Register';
+import Layout from './Layout/Layout';
 
 function App() {
   return (
     <Router>
-        <Routes>
-            
-        </Routes>
+        <Layout>
+            <Routes>
+                <Route path='/' element={<Login/>}/>
+                <Route path='/home' element={<Home/>}/>
+                <Route path='/register' element={<Register/>}/>
+            </Routes>
+        </Layout>
     </Router>
     
   );
