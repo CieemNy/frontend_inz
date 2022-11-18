3e832e1a21931ab82d61a4c87e4dfa765f23b707
Damian Banach <damian070799@wp.pl>
2022-11-18 11:40:43 +0100

    fetch actual logged user information

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,24 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-const Home = () => (
-    <div>
-        Home
-    </div>
-);
+const Home = () => {
+    const [userData, setUserData] = useState([]);
+    useEffect(()=>{
+        const getUser = async () => {
+            const {data: res} = await axios.get(`${process.env.REACT_APP_API_URL}/auth/users/me/`, {
+                headers: {
+                    'Authorization': `JWT ${localStorage.getItem('access')}`
+                }
+            })
+            setUserData(res);
+        }
+        getUser();
+    }, [])
+    return (
+        <div>
+            tes
+        </div>
+    );
+};
 
 export default Home;
