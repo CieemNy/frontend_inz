c977f6abd7b16f7af5409e6c3b35f8b31aff8490
Damian Banach <damian070799@wp.pl>
2022-11-28 18:16:21 +0100

    wip join team

--- src/Pages/Teams/id/TeamsDetail.js
+++ src/Pages/Teams/id/TeamsDetail.js
@@ -1,14 +1,17 @@
 import axios from 'axios';
 import React, {useEffect, useState} from 'react';
 import { useParams, Navigate } from 'react-router-dom';
-import { Box, Card, CardContent, Container, Typography } from '@mui/material';
+import { Box, Card, CardContent, Container, Typography, Divider, Button } from '@mui/material';
 import { connect } from 'react-redux';
-
+import Swal from 'sweetalert2';
 
 const CompanyDetail = ({isAuthenticated}) => {
     const {teamId} = useParams()
     const [team, setTeam] = useState([]);
+    const [members, setMembers] = useState([]);
     const [didFetch, setDidFetch] = useState(false);
+    const [join, setJoin] = useState(false);
+
     const getTeam = async () => {
         const {data: res} = await axios.get(`http://localhost:8000/accounts/teams/${teamId}`,{
             headers: {
@@ -17,13 +20,51 @@ const CompanyDetail = ({isAuthenticated}) => {
         })
         setTeam(res);
     }
+    const getMembers = async () => {
+        const {data: res} = await axios.get(`http://localhost:8000/accounts/teams/${teamId}/members`,{
+            headers: {
+                'Authorization': `JWT ${localStorage.getItem('access')}`
+            }
+        })
+        setMembers(res);
+    }
+    const joinTeam = async (e) => {
+        e.preventDefault();
+        try {
+          const res = await axios.post(`http://localhost:8000/accounts/teams/${teamId}/join`, {
+            headers: {
+              'Authorization': `JWT ${localStorage.getItem('access')}`
+          },
+        })
+        } catch (e) {
+          alert(e)
+          return false;
+        }
+        Swal.fire({
+            icon: 'success',
+            text: 'Dołączyłeś do zespołu!',
+        })
+        setJoin(true);
+    };
 
     useEffect(() => {
         if(!didFetch){
             setDidFetch(true)
             getTeam();
+            getMembers();
         }
     }, [didFetch])
+
+    const showJoinButton = () => {
+        return (
+            <>
+                <Divider sx={{marginTop: 2}}/>
+                <form onSubmit={e => joinTeam(e)}>
+                    <Button sx={{marginTop: 2}} variant="contained" type="submit">Dołącz</Button>
+                </form>
+            </>
+        );
+    }
 
     if (isAuthenticated===false) {
         return <Navigate to='/'/>
@@ -58,7 +99,38 @@ const CompanyDetail = ({isAuthenticated}) => {
 
                         }}
                     >
-                        
+                        <Typography>Nazwa Team'u</Typography>
+                        <Typography 
+                            variant='h5'
+                        >
+                            {team.name}
+                        </Typography>
+                        <Typography mt={2}>Lider</Typography>
+                        <Typography 
+                            variant='h5'
+                        >
+                            {team.leader}
+                        </Typography>
+                        <Typography mt={2}>Zajęte miejsca</Typography>
+                        <Typography 
+                            variant='h5'
+                        >
+                            {team.occupied_places}
+                        </Typography>
+                        <Typography mt={2}>Limit miejsc</Typography>
+                        <Typography
+                            variant='h5'
+                        >
+                            {team.places} 
+                        </Typography>
+                        <Divider sx={{marginTop: 2}}/>
+                        <Typography mt={2} variant='h5'>Członkowie</Typography>
+                        {members.map(members => (
+                            <Typography key={members.id} mt={2}>
+                                {members.member}
+                            </Typography>
+                        ))}
+                        {team.occupied_places !== team.places ? showJoinButton() : null}
                     </CardContent>
                 </Card>
             </Box>
