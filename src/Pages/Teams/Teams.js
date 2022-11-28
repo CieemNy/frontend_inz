d58e89f4f4feb196fb43ccbaa29d26d73c2fae96
Damian Banach <damian070799@wp.pl>
2022-11-28 13:14:44 +0100

    get teams from backend

--- src/Pages/Teams/Teams.js
+++ src/Pages/Teams/Teams.js
@@ -4,14 +4,37 @@ import { connect } from 'react-redux';
 import { Link, Navigate } from 'react-router-dom';
 import { Box, Card, CardContent, CardActions, Button, Container, Typography } from '@mui/material';
 
-const Teams = ({isAuthenticated}) => {
-  return (
-    <div>Teams</div>
-  );
+const Teams = ({isAuthenticated, isLeader, isMember}) => {
+    const [teams, setTeams] = useState([]);
+    const [didFetch, setDidFetch] = useState(false);
+    const getTeams = async () => {
+        const {data: res} = await axios.get(`http://localhost:8000/accounts/teams`,{
+            headers: {
+                'Authorization': `JWT ${localStorage.getItem('access')}`
+            }
+        })
+        setTeams(res);
+    }
+    useEffect(() => {
+        if(!didFetch){
+            setDidFetch(true)
+            getTeams();
+        }
+    }, [didFetch])
+
+    if (isAuthenticated===false) {
+        return <Navigate to='/'/>
+    }
+
+    return (
+        <div>Teams</div>
+    );
 }
 
 const mapStateToProps = state => ({
     isAuthenticated: state.auth.isAuthenticated,
+    isLeader: state.auth.user.is_leader,
+    isMember: state.auth.user.is_member
 });
 
 export default connect(mapStateToProps, {})(Teams);
